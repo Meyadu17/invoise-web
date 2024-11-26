dd50c70d47b5cc2e474e8df027ce11a4ce593480
mdelage <angie-island@hotmail.fr>
2024-11-26 16:26:47 +0100

    premiers pas formulaires

--- src/main/java/fr/apside/invoise/invoise_web/controller/InvoiceWebController.java
+++ src/main/java/fr/apside/invoise/invoise_web/controller/InvoiceWebController.java
@@ -5,33 +5,37 @@ import fr.apside.invoise.core.entity.Invoice;
 import fr.apside.invoise.core.service.InvoiceServiceInterface;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-// @Component indique que c'est une composant de l'application. Ici on utilise l'annotation fille :
+// @Component indique que c'est un composant de l'application. Ici on utilise l'annotation fille :
 @Controller// On par de stereotype
 @RequestMapping("/invoice")
 public class InvoiceWebController implements InvoiceControllerInterface {
 
-	// @Autowired permmet l'injection de dépendance sans passer par le setter
+	// @Autowired permet l'injection de dépendance sans passer par le setter
 	// @Resource est l'équivalent JEE
 	// @Inject est l'équivalent JEE 6
 	private final InvoiceServiceInterface invoiceServiceInterface;
 
-	// Constructeur : en passant InvoiceServiceInterface en final et en ajoutant le constructeur, l'application ne pourras démmarrer QUE si TOUTES les
-	// dépendances sont satisfaites.@Autowired deviens inutile.
+	// Constructeur : en passant InvoiceServiceInterface en final et en ajoutant le constructeur, l'application ne pourra démarrer QUE si TOUTES les
+	// dépendances sont satisfaites.@Autowired devient inutile.
 	public InvoiceWebController(InvoiceServiceInterface invoiceServiceInterface) {
 		this.invoiceServiceInterface = invoiceServiceInterface;
 	}
 
-	public void createInvoice(){
-		//Ici, on fait comme si on avait une interface graphique avec déjà les infos sais dans l'interface.
-		String customerName = "Tesla";
+	@PostMapping("")
+	// ModelAttribute va permettre d'instancier l'objet Invoice et de le fournir en entrée (équivaut à Invoice invoice=new Invoice();)
+	// ModelAttribute va donner un id à l'objet
+	public String createInvoice(@ModelAttribute Invoice invoice){
 
-		Invoice invoice=new Invoice();
-		invoice.setCustomerName(customerName);
 
 		invoiceServiceInterface.createInvoice(invoice);
+
+		return "invoice-created";
 	}
 
 	/*
@@ -45,11 +49,10 @@ public class InvoiceWebController implements InvoiceControllerInterface {
 	}
 	 */
 
-
 	//Une autre méthode :
 	//Si je laisse comme ça je suis obligé d'avoir invoice-home comme nom de fichier html. et
 	// Plus proche des standards actuels
-	@RequestMapping("/home")
+	@GetMapping("/home")
 	public String displayHome(Model model) {
 		System.out.println("La méthode display Home a été invoquée");
 
@@ -57,7 +60,7 @@ public class InvoiceWebController implements InvoiceControllerInterface {
 		return "invoice-home";
 	}
 
-	@RequestMapping("/{id}")
+	@GetMapping("/{id}")
 	public String displayInvoice(@PathVariable("id") String number, Model model) {
 		System.out.println("La méthode display Invoice a été invoquée");
 
@@ -65,5 +68,9 @@ public class InvoiceWebController implements InvoiceControllerInterface {
 		return "invoice-details";
 	}
 
+	@GetMapping("/create-form")
+	public String displayInvoiceCreateForm(@ModelAttribute Invoice invoice) {
+		return "invoice-create-form";
+	}
 
 }
